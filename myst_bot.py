0ef4b74cad398da408c5bf7a46ebdbfdf5cb98c6
RodrigoCV04 <cesarset.chavez@gmail.com>
2025-07-07 22:52:04 -0600

    Consulta a archivo

--- myst_bot.py
+++ myst_bot.py
@@ -3,6 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext, Ca
 from telegram.constants import ChatAction
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, LabeledPrice, ShippingOption
+import logging
 
 #Model
 from tools import ai_model as AI
@@ -14,9 +15,17 @@ WEB_APP_URL = "https://api-docs.deepseek.com/"
 
 bot = telebot.TeleBot(TOKEN_BOT)
 
-#Responses mold
-#buttons array of InlineKeyboardButton
-def SendResponseTemplate(chat_id, message:str , img_path:str="", buttons=[], markup_rows:int=0):
+
+#Solo mostrar WARNINGS y ERRORES
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.WARNING
+)
+
+
+#Plantilla para enviar respuestas
+#buttons: Arreglo de InlineKeyboardButton
+def SendResponseTemplate(chat_id, message:str , img_path:str="", buttons:list[str]=[], markup_rows:int=0):
     markup = None
     if len(buttons) > 0 and len(buttons) > 0:
         markup_rows = markup_rows if markup_rows <= 3 else 3
@@ -24,47 +33,77 @@ def SendResponseTemplate(chat_id, message:str , img_path:str="", buttons=[], mar
         markup.add(*buttons)
     
     if img_path != "":
-        bot.send_photo(chat_id=chat_id, caption=message, photo=img_path, reply_markup=markup, parse_mode='html')
+        return bot.send_photo(chat_id=chat_id, caption=message, photo=img_path, reply_markup=markup, parse_mode='html')
     else:
-        bot.send_message(chat_id=chat_id, text=message, reply_markup=markup, parse_mode='html')
+        return bot.send_message(chat_id=chat_id, text=message, reply_markup=markup, parse_mode='html')
 
 
 
-#Call back handlers
-async def CallbackHandlers(update: Update, ctx: CallbackContext):
+#Callback handlers
+def CallbackHandlers(update: Update, ctx: CallbackContext):
     query = update.callback_query
-    print(query)
+    callback = query.data
+    chat_id = query.message.chat.id
+    bot.answer_callback_query(callback_query_id=query.id)
+    
+    #Al presionar el botón Documentos
+    if callback == 'file_query':
+        file_path = "E:/Rod/Escuela/Universidad/SEM VI/Convocatoria_Movilidad.pdf"
+        place_holder = SendResponseTemplate(chat_id=chat_id, message='Analizando 🤔').id
+        res = AI.FileQuery('Que dice el documento?', file_path=file_path)
+        bot.delete_message(chat_id=chat_id, message_id=place_holder)
+        SendResponseTemplate(chat_id=chat_id, message=res)
+        
 
 
 
-#Commands
+#Comando start
 async def StartCommand(update: Update, ctx: CallbackContext):
     full_name = update.message.chat.full_name
     text = f'<a>Hola <b>{full_name}</b>! 👋\n \nBienvenido al chat con <b>MystBot</b>.</a>\n'
     text += f'<a>MystBot es un bot interactivo de inteligencia artificial en telegram 🤖</a>'
     buttons = [
-        InlineKeyboardButton('Ayuda 🆘', callback_data='HelpMsg'),
+        InlineKeyboardButton('Documento 📄', callback_data='file_query'),
         InlineKeyboardButton('Web 🌐', web_app=WebAppInfo(url=WEB_APP_URL))
     ]
     SendResponseTemplate(chat_id=update.message.chat_id, message=text, buttons=buttons, markup_rows=2)
 
 
 
+##Interacción sin contexto ni db
 async def TextHandler(update: Update, ctx: CallbackContext):
-    bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-    res = await AI.ChatModel(update.message.text)
-    print(res)
-    SendResponseTemplate(chat_id=update.message.chat_id, message=res)
+    chat_id = update.message.chat_id
+    bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
+    res = AI.ChatModel(query=update.message.text, prompt='Responder brevemente con 150 tokens')
+    SendResponseTemplate(chat_id=chat_id, message=res)
+
+
+
+#Interacción con contexto y db
+def ContextChat(db, memory, prompt):
+    async def TextContextHandler(update: Update, ctx: CallbackContext):
+        chat_id = update.message.chat_id
+        place_holder = SendResponseTemplate(chat_id=chat_id, message='Pensando... 🤔').id
+        res = AI.DBQueryMemory(db=db, query=update.message.text, memory=memory, prompt=prompt)
+        bot.delete_message(chat_id=chat_id, message_id=place_holder)
+        SendResponseTemplate(chat_id=chat_id, message=res)
+    return TextContextHandler
 
 
 
 def main():
-    print('Running')
+    docs = AI.SplitFile('E:/Rod/Escuela/Universidad/SEM VI/Convocatoria_Movilidad.pdf')
+    db = AI.CreateDB(docs=docs)
+    memory = AI.ConversationMemory()
+    prompt = AI.PromptTemplate(prompt="Eres un asistente cordial. Si el usuario saluda, agradece o hace una pregunta general, responde sin usar el documento. Si la pregunta es técnica o relacionada con el contenido, responde usando el contexto.")
+    
     app = ApplicationBuilder().token(TOKEN_BOT).build()
     app.add_handler(CommandHandler("start", StartCommand))
-    app.add_handler(MessageHandler(filters.TEXT, TextHandler))
+    app.add_handler(MessageHandler(filters.TEXT, ContextChat(db=db, memory=memory, prompt=prompt)))
     app.add_handler(CallbackQueryHandler(CallbackHandlers))
+    print('Running')
     app.run_polling()
+
 
 
 if __name__ == '__main__':
--- tools/ai_model.py
+++ tools/ai_model.py
@@ -1,44 +1,119 @@
-import asyncio
 from langchain_deepseek import ChatDeepSeek
+
 from langchain_unstructured import UnstructuredLoader
-#from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import  RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import ChatPromptTemplate
+
 
 
 DEEPSEEK_KEY = "sk-7b3c7cf39b354e1b8ba5258f633e8b94"
 MODEL = "deepseek-chat"
+API_URL = "https://api.deepseek.com/v1/chat/completions"
 
-async def ChatModel(text: str) -> str:
+
+#Hacer una pregunta básica
+def ChatModel(query: str, prompt:str) -> str:
     llm = ChatDeepSeek(api_key=DEEPSEEK_KEY, model=MODEL)
-    res = await asyncio.to_thread(llm.invoke, f'Responder brevemente con 150 tokens: {text}')
+    res = llm.invoke, f'{prompt}: {query}'
     return res.content
 
 
-async def LoadFile(file_url:str):
+
+def ConversationMemory() -> ConversationBufferMemory:
+    return ConversationBufferMemory(memory_key="chat_history", output_key="answer", return_messages=True) 
+
+
+
+#Plantilla para el prompt
+def PromptTemplate(prompt:str) -> ChatPromptTemplate:
+    return ChatPromptTemplate.from_messages([
+        ("system", f"{prompt}"),
+    ])
+
+
+
+#Separar un archivo
+def SplitFile(file_path:str):
     try:
-        loader = UnstructuredLoader(file_path=file_url)
-        pages = await asyncio.to_thread(loader.load)
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        text_splitter.split_documents(pages)
-        print(text_splitter)
-        return text_splitter
+        loader = UnstructuredLoader(file_path=file_path)
+        pages = loader.load()
+        return RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200).split_documents(pages)
     except Exception as e:
         print(f'ERR: Could not read the file \n{e}')
         return None
+            
 
 
-def CreateDB(docs):
-    embeddings = HuggingFaceBgeEmbeddings()
+#Separar un archivos
+def SliptFiles(files_path:list[str]) -> list:
+    files = []
+    for file_path in files_path:
+        try:
+            loader = UnstructuredLoader(file_path=file_path)
+            pages = loader.load()
+            files.append(RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200).split_documents(pages))
+        except Exception as e:
+            print(f'ERR: Could not read the file {file_path} \n{e}')
+    return files
+
+
+
+#Crea la DB
+def CreateDB(docs) -> FAISS:
+    embeddings = HuggingFaceEmbeddings()
     db = FAISS.from_documents(documents=docs, embedding=embeddings)
     db.save_local('deepseek_db')
     return db
 
 
-async def main():
-    respuesta = await LoadFile("C:/Users/cesar/Downloads/Grupos_SEM_2024-2.xlsx")
-    #print(respuesta)
 
-if __name__ == '__main__':
-    asyncio.run(main())
+#Consulta a la DB sin memoria
+def DBQuery(db:FAISS, query:str):
+    qa = RetrievalQA.from_chain_type(
+        llm=ChatDeepSeek(api_key=DEEPSEEK_KEY, model=MODEL),
+        chain_type="stuff",
+        retriever=db.as_retriever(search_kwargs={"k": 3}),
+        return_source_documents=True
+    )
+
+    res = qa.invoke({"query": query})
+    return res['result']
+
+
+
+#Consulta a la DB con memoria
+def DBQueryMemory(db:FAISS, query:str, memory:ConversationBufferMemory, prompt:ChatPromptTemplate):
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=ChatDeepSeek(api_key=DEEPSEEK_KEY, model=MODEL, temperature=0.3),
+        retriever=db.as_retriever(),
+        memory=memory,
+        return_source_documents=True,
+        condense_question_prompt=prompt
+    )
+
+    res = qa.invoke({"question": query})
+    return res['chat_history'][-1].content
+
+
+
+#Consulta sobre un archivo
+def FileQuery(query: str, file_path: str) -> str:
+    docs = SplitFile(file_path=file_path)
+    db = CreateDB(docs)
+    
+    qa = RetrievalQA.from_chain_type(
+        llm=ChatDeepSeek(api_key=DEEPSEEK_KEY, model=MODEL),
+        chain_type="stuff",
+        retriever=db.as_retriever(search_kwargs={"k": 3}),
+        return_source_documents=True
+    )
+
+    res = qa.invoke({"query": query})
+    return res['result']
+    
